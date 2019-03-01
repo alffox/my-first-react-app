4b5b6488b3175ed9de543d1982311a8f1da01ac3
alffox <alfonso.crisci.it@gmail.com>
2019-03-01 12:02:33 +0100

    refactor: Render a simple list as component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+//import './App.css';
+
+class ContactList extends React.Component {
+  render() {
+    const people = [
+      { name: 'Michael' },
+      { name: 'Ryan' },
+      { name: 'Tyler' }
+    ]
+
+    return <ol>
+      {people.map(person => (
+        <li key={person.name}>{person.name}</li>
+      ))}
+    </ol>
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-        <h2>Hello World !</h2>
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <ContactList/>
       </div>
     );
   }
